b03073223307f84468c26625eaddbb67b088bd20
KangCheolgu <opopqwqw123@gmail.com>
2023-06-08 11:45:04 +0900

    리액트 라우터 돔 준비

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function App() {
+  return(
+    <div>
+      <h1>안녕 리액트라우터돔</h1>
+      <Home></Home>
+      <Topics></Topics>
+      <Contact></Contact>
+    </div>
+  );
+}
+
+function Home() {
+  return(
+    <div>
+      <h1>홈</h1>
+      홈..
+    </div>
+
+  );
+}
+
+function Topics() {
+  return(
+    <div>
+      <h1>토픽</h1>
+     토픽..
+    </div>
+
+  );
+}
+function Contact() {
+  return(
+    <div>
+      <h1>콘텍트</h1>
+     콘텍트...
+    </div>
+
+  );
+}
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
